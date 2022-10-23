ae03ecb2ade2e9a7261b04af287ca0787bb6334f
ThanhDC18 <103198645+ThanhDC18@users.noreply.github.com>
2022-10-23 20:25:54 +0700

    Add files via upload

---
+++ PRN211_E4_Group6_A2/PRN211_E4_Group6_A2/GUI/CheckOutGUI.Designer.cs
@@ -0,0 +1,274 @@
+﻿namespace PRN211_E4_Group6_A2.GUI
+{
+    partial class CheckOutGUI
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.FirstName = new System.Windows.Forms.TextBox();
+            this.LastName = new System.Windows.Forms.TextBox();
+            this.Address = new System.Windows.Forms.TextBox();
+            this.City = new System.Windows.Forms.TextBox();
+            this.State = new System.Windows.Forms.TextBox();
+            this.Country = new System.Windows.Forms.TextBox();
+            this.Phone = new System.Windows.Forms.TextBox();
+            this.Email = new System.Windows.Forms.TextBox();
+            this.Total = new System.Windows.Forms.TextBox();
+            this.Save_button = new System.Windows.Forms.Button();
+            this.Cancel_button = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(70, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "First name:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(70, 89);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 15);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Last name:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(71, 146);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 15);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Address:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(71, 208);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(31, 15);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "City:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(71, 263);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(36, 15);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "State:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(71, 322);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(53, 15);
+            this.label6.TabIndex = 0;
+            this.label6.Text = "Country:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(71, 383);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(44, 15);
+            this.label7.TabIndex = 0;
+            this.label7.Text = "Phone:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(71, 446);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(39, 15);
+            this.label8.TabIndex = 0;
+            this.label8.Text = "Email:";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(71, 511);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(35, 15);
+            this.label9.TabIndex = 0;
+            this.label9.Text = "Total:";
+            // 
+            // FirstName
+            // 
+            this.FirstName.Location = new System.Drawing.Point(151, 26);
+            this.FirstName.Name = "FirstName";
+            this.FirstName.Size = new System.Drawing.Size(224, 23);
+            this.FirstName.TabIndex = 1;
+            // 
+            // LastName
+            // 
+            this.LastName.Location = new System.Drawing.Point(151, 86);
+            this.LastName.Name = "LastName";
+            this.LastName.Size = new System.Drawing.Size(224, 23);
+            this.LastName.TabIndex = 1;
+            // 
+            // Address
+            // 
+            this.Address.Location = new System.Drawing.Point(151, 143);
+            this.Address.Name = "Address";
+            this.Address.Size = new System.Drawing.Size(224, 23);
+            this.Address.TabIndex = 1;
+            // 
+            // City
+            // 
+            this.City.Location = new System.Drawing.Point(151, 205);
+            this.City.Name = "City";
+            this.City.Size = new System.Drawing.Size(224, 23);
+            this.City.TabIndex = 1;
+            // 
+            // State
+            // 
+            this.State.Location = new System.Drawing.Point(151, 260);
+            this.State.Name = "State";
+            this.State.Size = new System.Drawing.Size(224, 23);
+            this.State.TabIndex = 1;
+            // 
+            // Country
+            // 
+            this.Country.Location = new System.Drawing.Point(151, 319);
+            this.Country.Name = "Country";
+            this.Country.Size = new System.Drawing.Size(224, 23);
+            this.Country.TabIndex = 1;
+            // 
+            // Phone
+            // 
+            this.Phone.Location = new System.Drawing.Point(151, 380);
+            this.Phone.Name = "Phone";
+            this.Phone.Size = new System.Drawing.Size(224, 23);
+            this.Phone.TabIndex = 1;
+            // 
+            // Email
+            // 
+            this.Email.Location = new System.Drawing.Point(151, 443);
+            this.Email.Name = "Email";
+            this.Email.Size = new System.Drawing.Size(224, 23);
+            this.Email.TabIndex = 1;
+            // 
+            // Total
+            // 
+            this.Total.Location = new System.Drawing.Point(151, 508);
+            this.Total.Name = "Total";
+            this.Total.ReadOnly = true;
+            this.Total.Size = new System.Drawing.Size(224, 23);
+            this.Total.TabIndex = 1;
+            // 
+            // Save_button
+            // 
+            this.Save_button.Location = new System.Drawing.Point(131, 571);
+            this.Save_button.Name = "Save_button";
+            this.Save_button.Size = new System.Drawing.Size(75, 23);
+            this.Save_button.TabIndex = 2;
+            this.Save_button.Text = "Save";
+            this.Save_button.UseVisualStyleBackColor = true;
+            this.Save_button.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Cancel_button
+            // 
+            this.Cancel_button.Location = new System.Drawing.Point(278, 571);
+            this.Cancel_button.Name = "Cancel_button";
+            this.Cancel_button.Size = new System.Drawing.Size(75, 23);
+            this.Cancel_button.TabIndex = 3;
+            this.Cancel_button.Text = "Cancel";
+            this.Cancel_button.UseVisualStyleBackColor = true;
+            this.Cancel_button.Click += new System.EventHandler(this.Cancel_button_Click);
+            // 
+            // CheckOutGUI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(599, 657);
+            this.Controls.Add(this.Cancel_button);
+            this.Controls.Add(this.Save_button);
+            this.Controls.Add(this.Total);
+            this.Controls.Add(this.Email);
+            this.Controls.Add(this.Phone);
+            this.Controls.Add(this.Country);
+            this.Controls.Add(this.State);
+            this.Controls.Add(this.City);
+            this.Controls.Add(this.Address);
+            this.Controls.Add(this.LastName);
+            this.Controls.Add(this.FirstName);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "CheckOutGUI";
+            this.Text = "CheckOut";
+            this.Load += new System.EventHandler(this.CheckOut_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private Label label8;
+        private Label label9;
+        private TextBox FirstName;
+        private TextBox LastName;
+        private TextBox Address;
+        private TextBox City;
+        private TextBox State;
+        private TextBox Country;
+        private TextBox Phone;
+        private TextBox Email;
+        private TextBox Total;
+        private Button Save_button;
+        private Button Cancel_button;
+    }
+}
---
+++ PRN211_E4_Group6_A2/PRN211_E4_Group6_A2/GUI/CheckOutGUI.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using PRN211_E4_Group6_A2.Models;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.CompilerServices;
+using System.Net;
+using MusicStoreWin.Models;
+using Microsoft.Extensions.Configuration;
+
+namespace PRN211_E4_Group6_A2.GUI
+{
+    public partial class CheckOutGUI : Form
+    {
+        MusicStoreContext context;
+
+        public CheckOutGUI(int Id, MusicStoreContext context)
+        {
+            InitializeComponent();
+            FirstName.Text = FirstName.ToString();
+            LastName.Text = LastName.ToString();
+            Email.Text = Email.ToString();
+            this.context = context;
+            if (Id != -1)
+            {
+                Order order = context.Orders.Find(Total);
+                User user = context.Users.Find(Id);
+                FirstName.Text = user.FirstName;
+                LastName.Text = user.LastName;
+                Address.Text = user.Address;
+                City.Text = user.City;
+                State.Text = user.State;
+                Country.Text = user.Country;
+                Phone.Text = user.Phone;
+                Email.Text = user.Email;
+                Total.Text = Total.ToString();
+            }
+
+        }
+
+        private void CheckOut_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (int.Parse(FirstName.Text) == -1)
+            {
+                Order order = new Order
+                {
+                    FirstName = FirstName.Text,
+                    LastName = LastName.Text,
+                    Address = Address.Text,
+                    City = City.Text,
+                    State = State.Text,
+                    Country = Country.Text,
+                    Phone = Phone.Text,
+                    Email = Email.Text,
+                };
+                try
+                {
+                    context.Orders.Add(order);
+                    context.SaveChanges();
+                    MessageBox.Show("have been saved");
+                }
+                catch
+                {
+                    MessageBox.Show("Failed!");
+                }
+            }
+        }
+
+
+        private void Cancel_button_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
+
+  
+  
